b9469ed57ef4a474508783ed89bc8980b58147c8
Buya0720 <buyandelger0720@gmail.com>
2025-03-16 00:32:04 +1100

    tested integration of back_end + front_end

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
+
 const express = require('express');
+const path = require("path");
 const cors = require('cors');
 const emotionRoutes = require('./routes/emotionRoutes'); // Import emotion routes
-
 const mongoose = require("mongoose");
 
  
@@ -21,16 +22,47 @@ app.use(express.json());  // Middleware to parse JSON requests
 
 // Routes
 app.use('/api/emotions', emotionRoutes); // Use the emotion tracking routes
+app.use(express.static(path.resolve(__dirname, "../../public"))); // Serve static files
 
 // Sample route
 app.get('/', (req, res) => {
-    res.send('Hello, this is the back-end of your web app!');
+    res.sendFile(this.path.resolve(__dirname, '../public/checkHistory.html'));
+});
+
+app.get('/Questions', (req, res) => {
+    res.sendFile(this.path.resolve(__dirname, '../public/guidingQuestion.html'));
+});
+
+app.get('/identifyEmo', (req, res) => {
+    res.sendFile(this.path.resolve(__dirname, '../public/identifyEmo.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(this.path.resolve(__dirname, '../public/sirius_app/public/reflection.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(this.path.resolve(__dirname, '../public/sirius_app/public/show-gift'));
 });
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("✅ Connected to MongoDB Atlas"))
     .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
+//api to save user submission
+app.post("/log", async (req, res) => {
+    try {
+        const { eventId, selection, description, category, sliders } = req.body;
+
+        const newEvent = new Event({ eventId, selection, description, category, sliders });
+        await newEvent.save();
+
+        res.status(201).json({ message: "Event data saved successfully!" });
+    } catch (error) {
+        res.status(500).json({ error: "Error saving event data" });
+    }
+});
+    
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
