9180805c5ae1d07151486d137ead2a2d7e8df343
Casey You <z5484596@ad.unsw.edu.au>
2025-03-16 14:49:39 +1100

    docs: updated emotion list

--- backend/src/models/Emotion.js
+++ backend/src/models/Emotion.js
@@ -6,15 +6,16 @@ const allowedReactions = [
 ];
 
 const allowedEmotions = [
-    "Happy",
-    "Sad",
-    "Jealous"
+    "Happy", "Sad", "Excited", "Angry", "Nervous", "Calm", "Surprised", "Frustrated",
+    "Joyful", "Anxious", "Jealous", "Guilty", "Lonely", "Grateful", "Overwhelmed",
+    "Confident", "Embarrassed", "Proud", "Hopeful", "Regretful", "Rejected", "Worried",
+    "Loving", "Shy", "Disappointed", "Optimistic", "Resentful", "Hesitant"
 ];
 
 const emotionSchema = new mongoose.Schema({
     emotion: { type: String, enum: allowedEmotions, required: true },
     triggerEvent: { type: String, default: "" },
-    physicalReactions: { 
+    physicalReactions: {
         type: [String], // An array of strings
         enum: allowedReactions, // Restrict values to predefined list
         default: []
