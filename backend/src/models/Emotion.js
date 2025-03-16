46cd892cb392bde3ccfbcc9fb9dd49365c0fb3fe
Casey You <z5484596@ad.unsw.edu.au>
2025-03-16 11:16:44 +1100

    feat: modified schema for emotion records so that only field necessary is emotion itself

--- backend/src/models/Emotion.js
+++ backend/src/models/Emotion.js
@@ -13,11 +13,11 @@ const allowedEmotions = [
 
 const emotionSchema = new mongoose.Schema({
     emotion: { type: String, enum: allowedEmotions, required: true },
-    triggerEvent: { type: String, required: true },
+    triggerEvent: { type: String, default: "" },
     physicalReactions: { 
         type: [String], // An array of strings
         enum: allowedReactions, // Restrict values to predefined list
-        required: true
+        default: []
     },
     reflection: { type: String, default: null },
     timestamp: { type: Date, default: Date.now }
